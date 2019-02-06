52663194c3eb7ed5841179a1d770883b879c4fe4
sahil64 <47343566+sahil64@users.noreply.github.com>
2019-02-06 16:44:01 +0530

    Update index.php

--- production/index.php
+++ production/index.php
@@ -3,7 +3,7 @@ session_start();
 $city=$_REQUEST['city'];
 include('assets/config.nic.php');
 if (!isset($_SESSION['user'])) {
-    header("location: login.php?city=".$city);
+    //header("location: login.php?city=".$city);
 }
 
 /*
