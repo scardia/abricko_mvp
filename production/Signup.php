aeee810e9530d54f9f5041192d5880a50bbc5ff7
sahil64 <47343566+sahil64@users.noreply.github.com>
2019-02-14 13:25:43 +0530

    Update Signup.php

--- production/Signup.php
+++ production/Signup.php
@@ -16,7 +16,7 @@ if (isset($_POST['submit'])) {
         if ($result) {
             $to = $email;
             //$from = 'test@gtechinfo.com';
-            $from = 'Abricko Verify Mail <test@gtechinfo.com>';
+            $from = 'Abricko Verify Mail <mail@Abricko.com>';
             $headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
             $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
             $headers .= "MIME-Version: 1.0\r\n";
