5980803d7d6caa74d2d52fb2aa94eeb07f8e2848
Marco Montanari <sirmmo@gmail.com>
2019-03-12 17:01:49 +0100

    first up

--- production/main.php
+++ production/main.php
@@ -1,17 +1,19 @@
 <?php
     require_once('assets/config.nic.php');
-    header('Access-Control-Allow-Origin: *'); 
     try {
         if (isset($_REQUEST['qry']) && !empty($_REQUEST['qry'])) {
             $action = $_REQUEST['qry'];
         }
         //print_r($_POST);
         if ($action != '') {
+            header('Access-Control-Allow-Origin: *'); 
             echo $action();
         } else {
+            header('Access-Control-Allow-Origin: *'); 
             echo "Main class does not exists";
         }
     } catch (Exception $e) {
+        header('Access-Control-Allow-Origin: *'); 
         echo "Something is wrong! Please try again later!";
     }
 ?>
