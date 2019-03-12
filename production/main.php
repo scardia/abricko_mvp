d41bfe99aa01e74e710fc3009b40f431a649f68d
Marco Montanari <sirmmo@gmail.com>
2019-03-12 20:34:01 +0100

    up

--- production/main.php
+++ production/main.php
@@ -6,14 +6,11 @@
         }
         //print_r($_POST);
         if ($action != '') {
-            header('Access-Control-Allow-Origin: *'); 
             echo $action();
         } else {
-            header('Access-Control-Allow-Origin: *'); 
             echo "Main class does not exists";
         }
     } catch (Exception $e) {
-        header('Access-Control-Allow-Origin: *'); 
         echo "Something is wrong! Please try again later!";
     }
 ?>
